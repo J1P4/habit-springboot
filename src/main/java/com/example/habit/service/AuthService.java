8c781f5e116274cb1bfba9233367f6692d3bde89
Nachanjin <skxkswls@gmail.com>
2024-04-14 21:10:53 +0900

    feat : CI/CD 적용

--- src/main/java/com/example/habit/service/AuthService.java
+++ src/main/java/com/example/habit/service/AuthService.java
@@ -1,24 +1,17 @@
 package com.example.habit.service;
 
 
-import com.example.habit.constant.Constant;
 import com.example.habit.domain.User;
 import com.example.habit.dto.JwtTokenDto;
 import com.example.habit.dto.request.RegisterRequestDto;
 import com.example.habit.exception.CommonException;
 import com.example.habit.exception.ErrorCode;
 import com.example.habit.repository.UserRepository;
-import com.example.habit.type.EProvider;
 import com.example.habit.type.ERole;
-import com.example.habit.util.CookieUtil;
 import com.example.habit.util.JwtUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
