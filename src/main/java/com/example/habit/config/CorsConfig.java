ed1580b2051eead977f13eb36720f7a5082a1694
Nachanjin <skxkswls@gmail.com>
2024-04-15 15:25:42 +0900

    feat : cors 설정 추가

---
+++ src/main/java/com/example/habit/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.habit.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 허용할 오리진 설정
+        configuration.addAllowedOrigin("https://habit-fe-gyumongs-projects.vercel.app");
+        configuration.addAllowedOrigin("http://localhost:3000");
+
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+}
