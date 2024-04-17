eaf68bacb61f1ad258856efc6e36956e2c93dca7
Nachanjin <skxkswls@gmail.com>
2024-04-17 23:00:26 +0900

    feat : 음식 검색 API

--- src/main/java/com/example/habit/domain/Food.java
+++ src/main/java/com/example/habit/domain/Food.java
@@ -29,48 +29,52 @@ public class Food {
     @Column(name = "DetailClassification")
     private String detailClassification;
 
+    // 1회 제공량
+    @Column(name = "serving_size")
+    private int servingSize = 0;
+    //에너지
     @Column(name = "energy")
     private float energy = 0;
-
+    //수분
     @Column(name = "moisture")
     private float moisture = 0;
-
+    //단백질
     @Column(name = "protein")
     private float protein = 0;
-
+    //지방
     @Column(name = "fat")
     private float fat = 0;
-
+    //탄수화물
     @Column(name = "carbohydrate")
     private float carbohydrate = 0;
-
+    //식이섬유
     @Column(name = "dietary_fiber")
     private float dietaryFiber = 0;
-
+    //칼슘
     @Column(name = "calcium")
     private float calcium = 0;
-
+    //철분
     @Column(name = "iron")
     private float iron = 0;
-
+    //인
     @Column(name = "phosphorus")
     private float phosphorus = 0;
-
+    //셀레늄
     @Column(name = "selenium")
     private float selenium = 0;
-
+    //나트륨
     @Column(name = "sodium")
     private float sodium = 0;
-
+    //비타민A
     @Column(name = "vitamin_a")
     private float vitaminA = 0;
-
+    //비타민B1
     @Column(name = "vitamin_b1")
     private float vitaminB1 = 0;
-
+    //비타민B2
     @Column(name = "vitamin_b2")
     private float vitaminB2 = 0;
-
+    //비타민C
     @Column(name = "vitamin_c")
     private float vitaminC = 0;
 }
