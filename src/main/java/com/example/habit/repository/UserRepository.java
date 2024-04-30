f8e6b23a29fc572bf058ce9ab636d5bdf2dca64d
Sangmin Han <gkstkdals07@naver.com>
2024-05-01 03:55:30 +0900

    feat: 히스토리 내역에서 권장 영양소까지 반환하도록 함

--- src/main/java/com/example/habit/repository/UserRepository.java
+++ src/main/java/com/example/habit/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.habit.repository;
 import com.example.habit.domain.User;
 import com.example.habit.type.EProvider;
 import com.example.habit.type.ERole;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +16,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.socialId = :id AND u.eProvider = :provider")
     Optional<UserSecurityForm> findBySocialIdAndEProvider(@Param("id") String id, @Param("provider") EProvider provider);
+
+    @EntityGraph(attributePaths = {"userEssentialNutrients.essentialNutrients"})
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdWithUserEssentialNutrients(@Param("id")  Long Id);
 
     interface UserSecurityForm {
 
